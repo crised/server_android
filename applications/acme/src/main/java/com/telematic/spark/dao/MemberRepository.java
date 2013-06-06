da932bf6615f1404f3ab66544f1d3664e193886f
crised <crised@gmail.com>
2013-06-06 16:21:36 -0400

    little cleanup

--- applications/acme/src/main/java/com/telematic/spark/dao/MemberRepository.java
+++ applications/acme/src/main/java/com/telematic/spark/dao/MemberRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
