a318494feb14363a3e2fe7baee7ca7505442f83b
Russel <alexandre@russel.fr>
2013-06-07 14:25:17 +0200

    #3 using gson

--- applications/heron/heron/src/cl/telematic/heron/provider/TelematicHandler.java
+++ applications/heron/heron/src/cl/telematic/heron/provider/TelematicHandler.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.Collection;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -13,8 +15,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.util.Log;
-
 import cl.telematic.heron.Heron;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class TelematicHandler implements ResponseHandler {
     private static final String FLUSH_TIME = "5 minutes";
@@ -60,7 +64,7 @@ public class TelematicHandler implements ResponseHandler {
                     } while(next);
                 }
                 String where = sb.toString();
-                db.delete(TelematicMember.Members.MEMBER_NAME, where, null);
+                db.delete("member", where, null);
                 Log.d(Heron.LOG_TAG, "flushed old query results: " + c);
             }
         } finally {
@@ -79,24 +83,26 @@ public class TelematicHandler implements ResponseHandler {
             total.append(line);
         }
         String membersS = total.toString();
-        //TODO use GSON to parse the String
-        String[] members = membersS.split("\\},\\{");
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<Collection<Member>>(){}.getType();
+        Collection<Member> members = gson.fromJson(membersS, collectionType);
         ContentValues memberEntry;
-        int counter = 0;
-        for(String member: members) {
-            String id = member.substring(member.indexOf("\"id\":") + 5, member.indexOf(",\"name"));
-            String name = member.substring(member.indexOf("\"name\":") + 7, member.indexOf("\",\"email"));
-            String email = member.substring(member.indexOf("\"email\":") + 8, member.indexOf("\",\"phoneNumber"));
-            String phoneNumber = member.substring(member.indexOf("\"phoneNumber\":") + 15, member.indexOf("\"}"));
+        for(Member member: members) {
             memberEntry = new ContentValues();
-            memberEntry.put(TelematicMember.Members.MEMBER_ID, Integer.parseInt(id));
-            memberEntry.put(TelematicMember.Members.MEMBER_NAME, name);
-            memberEntry.put(TelematicMember.Members.EMAIL, email);
-            memberEntry.put(TelematicMember.Members.PHONE_NUMBER, phoneNumber);
+            memberEntry.put(TelematicMember.Members.MEMBER_ID, member.id);
+            memberEntry.put(TelematicMember.Members.MEMBER_NAME, member.name);
+            memberEntry.put(TelematicMember.Members.EMAIL, member.email);
+            memberEntry.put(TelematicMember.Members.PHONE_NUMBER, member.phoneNumber);
             SQLiteDatabase db = telematicContentProvider.getDatabase();
             telematicContentProvider.insert(TelematicMember.Members.MEMBERS_URI, memberEntry, db);
-            counter++;
         }
-        return counter;
+        return members.size();
+    }
+    
+    private static class Member {
+        private int id;
+        private String name;
+        private String email;
+        private String phoneNumber;
     }
 }
